c6117d86139a6109ba9eb314e6bd33813c8cbd0e
Samuele Lorefice <aironenerowork@gmail.com>
2016-04-29 14:00:10 +0200

    removed entries, will break previus cfg.json files

--- Core/Objects/BotSetup.cs
+++ Core/Objects/BotSetup.cs
@@ -28,30 +28,5 @@ namespace TelegramSharp.Core.Objects {
 		/// </summary>
 		/// <value>The bot token.</value>
 		[DataMember] public string BotToken{ get; set; }
-
-		/// <summary>
-		/// Gets or sets a value indicating whether this <see cref="Core.Objects.BotSetup"/> Q chat answers enabled.
-		/// </summary>
-		/// <value><c>true</c> if Q chat answers enabled; otherwise, <c>false</c>.</value>
-		[DataMember] public bool QChatAnswersEnabled{ get; set; }
-
-		/// <summary>
-		/// Gets or sets the owner identifier.
-		/// </summary>
-		/// <value>The owner identifier.</value>
-		[DataMember] public int OwnerId{ get; set; }
-
-		/// <summary>
-		/// Gets or sets a value indicating whether this <see cref="Core.Objects.BotSetup"/> me enabled.
-		/// </summary>
-		/// <value><c>true</c> if me enabled; otherwise, <c>false</c>.</value>
-		[DataMember] public bool MeEnabled{ get; set; }
-
-		/// <summary>
-		/// Gets or sets a value indicating whether this <see cref="Core.Objects.BotSetup"/> bot stats enabled.
-		/// </summary>
-		/// <value><c>true</c> if bot stats enabled; otherwise, <c>false</c>.</value>
-		[DataMember] public bool BotStatsEnabled{ get; set; }
-
 	}
 }
